39b8bbb1efe2204e8f2b8ca577c9b3da809f1c77
CodeUncas <peruzziuncas@gmail.com>
2025-03-05 16:41:05 +0100

    Indentation Errors

--- SimulationModule/Tests/test_GraphWrapper.py
+++ SimulationModule/Tests/test_GraphWrapper.py
@@ -18,27 +18,27 @@ class TestGraphWrapper(unittest.TestCase):
             self.network_type
         )
 
-def test_initialization(self):
-    
-    with patch('osmnx.graph_from_point') as mock_graph_from_point:
-        self.graph_wrapper.get_graph()
+    def test_initialization(self):
+        
+        with patch('osmnx.graph_from_point') as mock_graph_from_point:
+            self.graph_wrapper.get_graph()
+            
+            mock_graph_from_point.assert_called_once_with(
+                (self.latitude, self.longitude),
+                dist=self.map_radius,
+                network_type=self.network_type
+            )
+
+    @patch('osmnx.graph_from_point')
+    def test_get_graph(self, mock_graph_from_point):
+        mock_graph = MagicMock()
+        mock_graph_from_point.return_value = mock_graph
+
+        result = self.graph_wrapper.get_graph()
         
         mock_graph_from_point.assert_called_once_with(
             (self.latitude, self.longitude),
             dist=self.map_radius,
             network_type=self.network_type
         )
-
-@patch('osmnx.graph_from_point')
-def test_get_graph(self, mock_graph_from_point):
-    mock_graph = MagicMock()
-    mock_graph_from_point.return_value = mock_graph
-
-    result = self.graph_wrapper.get_graph()
-    
-    mock_graph_from_point.assert_called_once_with(
-        (self.latitude, self.longitude),
-        dist=self.map_radius,
-        network_type=self.network_type
-    )
-    self.assertEqual(result, mock_graph)
+        self.assertEqual(result, mock_graph)
