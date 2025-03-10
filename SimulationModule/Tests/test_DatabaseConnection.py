9b12d46067f763f3cc7d910ab79f49ce909d21ab
Sadixo <manuelgusella13@gmail.com>
2025-03-10 20:52:23 +0100

    Modificati vecchi test e implementati i test per le nuovi classi create

---
+++ SimulationModule/Tests/test_DatabaseConnection.py
@@ -0,0 +1,38 @@
+import unittest
+from Models.DatabaseConnection import DatabaseConnection
+from Models.DatabaseConfigParameters import DatabaseConfigParameters
+import json
+from pathlib import Path
+import clickhouse_connect
+from unittest.mock import MagicMock,patch
+
+class TestDatabaseConnection(unittest.TestCase):
+
+    def setUp(self):
+        self.config_parameters = DatabaseConfigParameters()
+        
+        self.db_connection = DatabaseConnection(self.config_parameters)
+
+
+    @patch('clickhouse_connect.get_client')
+    def test_connect(self, mock_get_client):
+        mock_client_instance = mock_get_client.return_value
+        
+        result = self.db_connection.connect()
+
+        mock_get_client.assert_called_once_with(
+            host=self.config_parameters.host,
+            port=self.config_parameters.port,
+            username=self.config_parameters.user,
+            password=self.config_parameters.password
+        )
+        self.assertEqual(result, mock_client_instance)
+        
+
+    @patch('clickhouse_connect.get_client')
+    def test_disconnect(self, mock_get_client):
+        mock_client_instance = mock_get_client.return_value
+
+        self.db_connection.disconnect()
+        mock_client_instance.close.assert_called_once()
+        self.assertIsNone(self.db_connection.connection)
--- SimulationModule/Tests/test_SensorFactory.py
+++ SimulationModule/Tests/test_SensorFactory.py
@@ -1,17 +1,40 @@
 import unittest
 from Models.SensorSubject import SensorSubject
 from Models.GpsSensor import GpsSensor
+from Models.UserSensorService import UserSensorService
+from Models.ISensorRepository import ISensorRepository
+from Models.IUserRepository import IUserRepository
 from Models.SensorFactory import SensorFactory
+from unittest.mock import Mock, patch
 import uuid 
 
 class TestSensorFactory(unittest.TestCase):
+    def setUp(self):
+        self.mock_sensor_repository = Mock(spec=ISensorRepository)
+        self.mock_user_repository = Mock(spec=IUserRepository)
+        self.mock_user_sensor_service=Mock(spec=UserSensorService)
+
+        self.patcher = patch('Models.SensorFactory.UserSensorService', return_value=self.mock_user_sensor_service)
+        self.patcher.start()
+
+        self.sensor_factory = SensorFactory(self.mock_sensor_repository, self.mock_user_repository)
+        
+    def tearDown(self):
+        # it stops the patcher after every test
+        self.patcher.stop()
+
     def test_create_gps_sensor_correct_type(self):
-        sensor_istance = SensorFactory.create_gps_sensor()
+        mock_uuid = "mock-uuid-123"
+        self.mock_user_sensor_service.assign_sensor_to_user.return_value = mock_uuid
+
+        sensor_istance = self.sensor_factory.create_gps_sensor()
         self.assertIsInstance(sensor_istance, SensorSubject)
         self.assertIsInstance(sensor_istance, GpsSensor)
     
     def test_create_gps_sensor_valid_uuid(self):
-        sensor_istance = SensorFactory.create_gps_sensor()
+        mock_uuid = "mock-uuid-123"
+        self.mock_user_sensor_service.assign_sensor_to_user.return_value = mock_uuid
+        sensor_istance = self.sensor_factory.create_gps_sensor()
         self.assertIsNotNone(sensor_istance.get_sensor_uuid())
         sensor_uuid = sensor_istance.get_sensor_uuid()
 
@@ -21,7 +44,10 @@ class TestSensorFactory(unittest.TestCase):
     
     def test_sensor_has_unique_uuid(self):
         num_sensor = 10
-        sensors_list = [SensorFactory.create_gps_sensor() for _ in range(num_sensor)]
+        mock_uuids = [f"mock-uuid-{i}" for i in range(num_sensor)]
+        self.mock_user_sensor_service.assign_sensor_to_user.side_effect = mock_uuids
+
+        sensors_list = [self.sensor_factory.create_gps_sensor() for _ in range(num_sensor)]
         sensors_uuid_list = [sensor.get_sensor_uuid() for sensor in sensors_list]
         #set contains only different elements, so if len corresponds it means they are unique
         self.assertEqual(len(sensors_uuid_list), len(set(sensors_uuid_list)))
