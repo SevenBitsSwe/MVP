4477b23d12729fc67868f54e6167b01005f680e8
Sadixo <manuelgusella13@gmail.com>
2025-03-17 14:57:02 +0100

    Aggiornati file sql

--- StorageData/interesseUtente.sql
+++ StorageData/interesseUtente.sql
@@ -1,12 +1,29 @@
-CREATE TABLE nearyou.interesseUtente(
-       utente UInt32,
-       interesse String,
-       PRIMARY KEY(utente, interesse)
+CREATE TABLE nearyou.user_interest(
+       user_uuid UUID,
+       interest String,
+       PRIMARY KEY(user_uuid, interest)
 ) ENGINE = MergeTree()
-ORDER BY (utente, interesse);
+ORDER BY (user_uuid, interest);
 
 
-
-INSERT INTO nearyou.interesseUtente (utente, interesse) VALUES
-(1, 'Sport'),
-(1, 'Natura');
+INSERT INTO nearyou.user_interest (user_uuid, interest) VALUES
+('a64d2c83-4e4e-4548-9214-515e11c0453e', 'Sport'), 
+('a64d2c83-4e4e-4548-9214-515e11c0453e', 'Natura'),
+('48872095-944a-4a14-8832-d4d9c9996960', 'Ristorazione'),
+('48872095-944a-4a14-8832-d4d9c9996960', 'Cultura'),
+('af6e3f66-7732-4363-9957-9d418070f3ea', 'Musica'),
+('af6e3f66-7732-4363-9957-9d418070f3ea', 'Viaggi'),
+('b44c9031-9f70-4094-b19d-c4d7dc69801a', 'Tecnologia'),
+('b44c9031-9f70-4094-b19d-c4d7dc69801a', 'Cinema'),
+('2a39b15a-285a-40bb-912c-98181e781eab', 'Moda'),
+('2a39b15a-285a-40bb-912c-98181e781eab', 'Arte'),
+('9c7cfb9e-95a8-4dcb-82c8-2c22384f614e', 'Fitness'),
+('9c7cfb9e-95a8-4dcb-82c8-2c22384f614e', 'Sport'),
+('815a28d4-c66d-4b71-a84c-c6fdffdd17ca', 'Natura'),
+('815a28d4-c66d-4b71-a84c-c6fdffdd17ca', 'Fotografia'),
+('b21fe281-421c-4edf-955a-910d4aaff7d4', 'Teatro'),
+('b21fe281-421c-4edf-955a-910d4aaff7d4', 'Cultura'),
+('aac055b2-b691-460b-9b93-68a9c9c70ed7', 'Educazione'),
+('aac055b2-b691-460b-9b93-68a9c9c70ed7', 'Musica'),
+('714e07c1-5b72-43a1-994e-d60e0d1eda6d', 'Giochi'),
+('714e07c1-5b72-43a1-994e-d60e0d1eda6d', 'Cinema');;
--- StorageData/utente.sql
+++ StorageData/utente.sql
@@ -12,15 +12,14 @@ CREATE TABLE IF NOT EXISTS nearyou.user(
 ORDER BY user_uuid;
 
 
-
 INSERT INTO nearyou.user (user_uuid, assigned_sensor_uuid, name, surname, email, gender, birthdate, civil_status) VALUES
-(generateUUIDv4(), NULL, 'Mario', 'Rossi', 'mario.rossi@example.com', 'M', '1985-05-15', 'Single'),
-(generateUUIDv4(), NULL, 'Laura', 'Bianchi', 'laura.b@example.com', 'F', '1990-03-22', 'Married'),
-(generateUUIDv4(), NULL, 'Giuseppe', 'Verdi', 'g.verdi@example.com', 'M', '1975-11-30', 'Married'),
-(generateUUIDv4(), NULL, 'Anna', 'Ferrari', 'a.ferrari@example.com', 'F', '1988-07-18', 'Single'),
-(generateUUIDv4(), NULL, 'Marco', 'Romano', 'm.romano@example.com', 'M', '1982-09-25', 'Divorced'),
-(generateUUIDv4(), NULL, 'Sofia', 'Marino', 's.marino@example.com', 'F', '1995-12-03', 'Single'),
-(generateUUIDv4(), NULL, 'Luca', 'Costa', 'l.costa@example.com', 'M', '1980-04-10', 'Married'),
-(generateUUIDv4(), NULL, 'Elena', 'Ricci', 'e.ricci@example.com', 'F', '1992-08-28', 'Single'),
-(generateUUIDv4(), NULL, 'Paolo', 'Conti', 'p.conti@example.com', 'M', '1987-01-15', 'Married'),
-(generateUUIDv4(), NULL, 'Chiara', 'Mancini', 'c.mancini@example.com', 'F', '1993-06-20', 'Single');
+('a64d2c83-4e4e-4548-9214-515e11c0453e', NULL, 'Mario', 'Rossi', 'mario.rossi@example.com', 'M', '1985-05-15', 'Single'),
+('48872095-944a-4a14-8832-d4d9c9996960', NULL, 'Laura', 'Bianchi', 'laura.b@example.com', 'F', '1990-03-22', 'Married'),
+('af6e3f66-7732-4363-9957-9d418070f3ea', NULL, 'Giuseppe', 'Verdi', 'g.verdi@example.com', 'M', '1975-11-30', 'Married'),
+('b44c9031-9f70-4094-b19d-c4d7dc69801a', NULL, 'Anna', 'Ferrari', 'a.ferrari@example.com', 'F', '1988-07-18', 'Single'),
+('2a39b15a-285a-40bb-912c-98181e781eab', NULL, 'Marco', 'Romano', 'm.romano@example.com', 'M', '1982-09-25', 'Divorced'),
+('9c7cfb9e-95a8-4dcb-82c8-2c22384f614e', NULL, 'Sofia', 'Marino', 's.marino@example.com', 'F', '1995-12-03', 'Single'),
+('815a28d4-c66d-4b71-a84c-c6fdffdd17ca', NULL, 'Luca', 'Costa', 'l.costa@example.com', 'M', '1980-04-10', 'Married'),
+('b21fe281-421c-4edf-955a-910d4aaff7d4', NULL, 'Elena', 'Ricci', 'e.ricci@example.com', 'F', '1992-08-28', 'Single'),
+('aac055b2-b691-460b-9b93-68a9c9c70ed7', NULL, 'Paolo', 'Conti', 'p.conti@example.com', 'M', '1987-01-15', 'Married'),
+('714e07c1-5b72-43a1-994e-d60e0d1eda6d', NULL, 'Chiara', 'Mancini', 'c.mancini@example.com', 'F', '1993-06-20', 'Single');
